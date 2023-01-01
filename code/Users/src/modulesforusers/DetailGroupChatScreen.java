a6c3c4683493921006a59e77489efe1ad0686794
Fox24 <73237980+duy2404@users.noreply.github.com>
2023-01-01 18:58:48 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/Dutus244/JavaFinalProject""
    This reverts commit eeb490e66f9b2e904b96fdf760cd21438199bcc9.

--- code/Users/src/modulesforusers/DetailGroupChatScreen.java
+++ code/Users/src/modulesforusers/DetailGroupChatScreen.java
@@ -9,20 +9,24 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.MessageFormat;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
 
 public class DetailGroupChatScreen extends JFrame {
 
-	JPanel contentPane;
+	JLayeredPane contentPane;
 
 	private JTable adminTable;
 	private DefaultTableModel adminTableModel;
@@ -58,31 +62,78 @@ public class DetailGroupChatScreen extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400,530);
 		
-		contentPane = new JPanel();
+		contentPane = new JLayeredPane();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLocation(0, 0);
 		contentPane.setLayout(null);
 		
 		setContentPane(contentPane);
 		
+		//group image
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		btnNewButton.setBounds(175, 25, 50, 50);
-		contentPane.add(btnNewButton);
+		contentPane.add(btnNewButton, JLayeredPane.DEFAULT_LAYER);
 		
-		JLabel lbl_1 = new JLabel("New labelllllllllllllllllllllll", SwingConstants.CENTER);
+		//group name
+		JLabel lbl_1 = new JLabel("bucubucubucubucubucu", SwingConstants.CENTER);
 		lbl_1.setBounds(50, 85, 300, 15);
-		contentPane.add(lbl_1);
+		contentPane.add(lbl_1, JLayeredPane.DEFAULT_LAYER);
 		
+		JTextField tf = new JTextField(15);
+		tf.setBounds(125, 85, 150, 15);
+		
+		JButton btnConfirm = new JButton("New button");
+		btnConfirm.setBounds(280, 85, 15, 15);
+		
+		JButton btnCancel = new JButton("New button");
+		btnCancel.setBounds(300, 85, 15, 15);
+		
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tf.setVisible(false);
+				btnConfirm.setVisible(false);
+				btnCancel.setVisible(false);
+			}
+		});
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tf.setVisible(false);
+				btnConfirm.setVisible(false);
+				btnCancel.setVisible(false);
+			}
+		});
+		
+		
+		contentPane.add(tf, JLayeredPane.PALETTE_LAYER);
+		contentPane.add(btnConfirm, JLayeredPane.PALETTE_LAYER);
+		contentPane.add(btnCancel, JLayeredPane.PALETTE_LAYER);
+		
+		tf.setVisible(false);
+		btnConfirm.setVisible(false);
+		btnCancel.setVisible(false);
+		
+		JButton btnChangeName = new JButton("Change name");
+		btnChangeName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tf.setVisible(true);
+				btnConfirm.setVisible(true);
+				btnCancel.setVisible(true);
+			}
+		});
+		btnChangeName.setBounds(135, 105, 120, 20);
+		contentPane.add(btnChangeName, JLayeredPane.DEFAULT_LAYER);
+		
+		//admin
 		JLabel lbl_2 = new JLabel("Admin list:");
-		lbl_2.setBounds(100, 120, 100, 15);
-		contentPane.add(lbl_2);
+		lbl_2.setBounds(100, 130, 100, 15);
+		contentPane.add(lbl_2, JLayeredPane.DEFAULT_LAYER);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(100, 140, 200, 100);
+		scrollPane_1.setBounds(100, 150, 200, 100);
 		scrollPane_1.setViewportView(adminTable);
 		String[] columnNames_1 = { "Username"};
 		adminTableModel = new DefaultTableModel(columnNames_1, 0) {
@@ -96,14 +147,15 @@ public class DetailGroupChatScreen extends JFrame {
 		adminTable = new JTable();
 		adminTable.setModel(adminTableModel);
 		
-		contentPane.add(scrollPane_1);
+		contentPane.add(scrollPane_1, JLayeredPane.DEFAULT_LAYER);
 		
+		//member
 		JLabel lbl_3 = new JLabel("Member list:");
-		lbl_3.setBounds(100, 250, 100, 15);
+		lbl_3.setBounds(100, 260, 100, 15);
 		contentPane.add(lbl_3);
 		
 		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(100, 270, 200, 200);
+		scrollPane_2.setBounds(100, 280, 200, 200);
 		scrollPane_2.setViewportView(adminTable);
 		String[] columnNames_2 = { "Username" };
 		memberTableModel = new DefaultTableModel(columnNames_2, 0) {
@@ -117,7 +169,7 @@ public class DetailGroupChatScreen extends JFrame {
         memberTable = new JTable();
         memberTable.setModel(memberTableModel);
 		
-		contentPane.add(scrollPane_2);
+		contentPane.add(scrollPane_2, JLayeredPane.DEFAULT_LAYER);
 	}
 	private static Object getGroupName(String groupID) throws SQLException{
 		// Connect DB
@@ -134,7 +186,9 @@ public class DetailGroupChatScreen extends JFrame {
 		}
 		
 		stmt = conn.createStatement();
-		String sql = "";
+		String sql = "Select * from inbox "
+				+ "where inbox.TypeInbox = 'group' "
+				+ "and inbox.InboxID ='" + groupID + "' ";
 
 		ResultSet rs = stmt.executeQuery(sql);
 
@@ -157,7 +211,7 @@ public class DetailGroupChatScreen extends JFrame {
 			e1.printStackTrace();
 		}
 		
-		return rs.getString("?????");
+		return rs.getString("InboxName");
 	}
 	private static void changeGroupName(String groupID, String newName) throws SQLException{
 		// Connect DB
@@ -174,9 +228,12 @@ public class DetailGroupChatScreen extends JFrame {
 		}
 		
 		stmt = conn.createStatement();
-		String sql = "";
+		String sql = MessageFormat.format("update users "
+				+ "set InboxName = \"{0}\""
+				+ "where InboxID = \"{1}\"", 
+				newName, groupID);
 
-		ResultSet rs = stmt.executeQuery(sql);
+		stmt.executeQuery(sql);
 
 		try {
 			stmt.close();
@@ -191,7 +248,7 @@ public class DetailGroupChatScreen extends JFrame {
 			e1.printStackTrace();
 		}
 	}
-	private static Vector<Vector<Object>> getAdminGroup(String groupID) throws SQLException{
+	private static Vector<Vector<Object>> getGroupChatAdmin(String groupID) throws SQLException{
 		// Connect DB
 		Connection conn = null;
 		Statement stmt = null;
@@ -206,8 +263,12 @@ public class DetailGroupChatScreen extends JFrame {
 		}
 		
 		stmt = conn.createStatement();
-		String sql = "";
-
+		String sql = "select * from users "
+				+ "join admingroup on users.UserID = admingroup.UserID "
+				+ "join inbox on admingroup.InboxID = inbox.InboxID "
+				+ "and inbox.TypeInbox = 'group' "
+				+ "and inbox.InboxID ='" + groupID + "' ";
+		
 		ResultSet rs = stmt.executeQuery(sql);
 		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
 		while (rs.next()) { 
@@ -238,7 +299,7 @@ public class DetailGroupChatScreen extends JFrame {
 		
 		return data;
 	}
-	private static Vector<Vector<Object>> getMemberGroup(String groupID) throws SQLException{
+	private static Vector<Vector<Object>> getGroupChatMember(String groupID) throws SQLException{
 		// Connect DB
 		Connection conn = null;
 		Statement stmt = null;
@@ -253,13 +314,16 @@ public class DetailGroupChatScreen extends JFrame {
 		}
 		
 		stmt = conn.createStatement();
-		String sql = "";
-
+		String sql = "select * from users "
+				+ "join inboxparticipants on users.UserID = inboxparticipants.UserID "
+				+ "join inbox on inboxparticipants.InboxID = inbox.InboxID "
+				+ "and inbox.TypeInbox = 'group' "
+				+ "and + " + groupID + " = inbox.InboxID";
+		
 		ResultSet rs = stmt.executeQuery(sql);
 		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
 		while (rs.next()) { 
 			Vector<Object> row = new Vector<Object>();
-			row.add(rs.getString("UserName"));
 			row.add(rs.getString("UserName"));
 			
 			data.add(row);
--- code/Users/src/modulesforusers/FriendRequest.java
+++ code/Users/src/modulesforusers/FriendRequest.java
@@ -8,24 +8,22 @@ import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+
 import javax.swing.JButton;
 import javax.swing.Icon;
 import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,12 +31,8 @@ import javax.swing.border.LineBorder;
 import javax.swing.ImageIcon;
 
 public class FriendRequest extends JFrame implements ActionListener {
-	JPanel panel_6;
-	JLabel lblNewJgoodiesLabel_numberInvitation;
-	private JPanel contentPane;
-	private JTextField textField;
-//	boolean ThreadContinue = true;
-	JPanel panelRequest;
+	
+	Container container;
 	JButton buttonChatMenu;
 	JButton buttonFriendMenu;
 	JButton buttonSettingMenu;
@@ -48,8 +42,10 @@ public class FriendRequest extends JFrame implements ActionListener {
 	JButton btnGroup;
 	String username;
 	Socket client;
+	JPanel panelView;
 	List<String> usernameRequest = new ArrayList<>();
 	public FriendRequest(String Username, Socket s) {
+		System.out.println("F request");
 		this.username = Username;
 		this.client = s;
 		
@@ -66,6 +62,9 @@ public class FriendRequest extends JFrame implements ActionListener {
 	Connection conn = null;
 	java.sql.Statement stmt;
 	ResultSet rs;
+	Connection connd = null;
+	java.sql.Statement stmtd;
+	ResultSet rsd;
 	boolean isOpen = true;
 	class ReadFriendRequest extends Thread{
 		public void run() {
@@ -91,24 +90,70 @@ public class FriendRequest extends JFrame implements ActionListener {
 				
 
 				try {
+					remove(panelView);
 					usernameRequest.clear();
 					rs = ((java.sql.Statement)stmt).executeQuery("SELECT UserName FROM users where UserID in (select UserID FROM sendrequestfriendlistfriendlist where AddFriendID =  '"+ username +"')");
-				
 					while(rs.next()) {
 						usernameRequest.add(rs.getString("UserName"));
 					}
-					System.out.println(usernameRequest.size());
+					System.out.println("list" +usernameRequest.size());
 					
-					JPanel panel_9 = new JPanel();
-					panel_9.setBounds(10, 94, 586, 41);
-					panel_6.add(panel_9);
-					JLabel lblNewJgoodiesLabel_13 = DefaultComponentFactory.getInstance().createLabel("Invitation("+usernameRequest.size()+")");
-					lblNewJgoodiesLabel_13.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-					panel_9.add(lblNewJgoodiesLabel_13);
+					panelView = new JPanel();
+			        panelView.setLayout(null);
+			      
+			        panelView.setPreferredSize(new Dimension(596,600));
+			        
+			        JPanel panelOptionTitle = new JPanel();
+			        panelOptionTitle.setLayout(null);
+			        panelOptionTitle.setBorder(new LineBorder(new Color(0, 0, 0)));
+			        panelOptionTitle.setBounds(0, 0, 596, 80);
+			        panelView.add(panelOptionTitle);
+			        
+			        JLabel lbOptionTitle = new JLabel("Friend's Request");
+			        lbOptionTitle.setFont(new Font("Times New Roman", Font.BOLD, 30));
+			        lbOptionTitle.setBounds(10, 11, 351, 60);
+			        panelOptionTitle.add(lbOptionTitle);
+			        
+			        JPanel panelViewAdvance = new JPanel();
+			        panelViewAdvance.setBounds(10, 94, 586, 41);
+			        panelView.add(panelViewAdvance);
+			        
+			        
+			        JLabel lbInvitation = new JLabel("Invitation("+usernameRequest.size()+")");
+			        lbInvitation.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			        panelViewAdvance.add(lbInvitation);
+			        
+			        JPanel panelListUser = new JPanel();
+			        panelListUser.setLayout(null);
+			        panelListUser.setBounds(0, 134, 582, 429);
+			        panelView.add(panelListUser);
 					
-//					for(int i =0; i<usernameRequest.size();i++ ) {
-//						
-//					}
+			        for(int i =0; i<usernameRequest.size();i++ ) {
+			        	
+			        	JPanel panelFriendInfo = new JPanel();
+			            panelFriendInfo.setLayout(null);
+			            panelFriendInfo.setBorder(new LineBorder(new Color(0, 0, 0)));
+			            panelFriendInfo.setBounds(10, 11+ i*105, 576, 94);
+			            panelListUser.add(panelFriendInfo);
+			            
+			            JLabel lbFriendUsername = new JLabel(usernameRequest.get(i));
+			            lbFriendUsername.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+			            lbFriendUsername.setBounds(10, 21, 177, 49);
+			            panelFriendInfo.add(lbFriendUsername);
+			            
+			            JButton btnAccept = new JButton("ACCEPT");
+			            btnAccept.setFont(new Font("Tahoma", Font.BOLD, 16));
+			            btnAccept.setBounds(430, 11, 136, 32);
+			            panelFriendInfo.add(btnAccept);
+			            
+			            btnAccept.addActionListener(event -> AcceptHandle(event,lbFriendUsername.getText()));
+			            JButton btnChat = new JButton("CHAT");
+			            btnChat.setFont(new Font("Tahoma", Font.BOLD, 16));
+			            btnChat.setBounds(430, 51, 136, 32);
+			            panelFriendInfo.add(btnChat);
+		
+			        }
+			        getContentPane().add(panelView, BorderLayout.EAST);
 					validate();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
@@ -122,30 +167,63 @@ public class FriendRequest extends JFrame implements ActionListener {
 				}
 
 			}
-			
 		}
 	}
+	private void AcceptHandle(ActionEvent e, String acceptUser) {
+		System.out.println("acceptHandle, " + acceptUser);
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			connd = DriverManager.getConnection(Main.DB_URL, Main.USER, Main.PASS);
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			stmtd = conn.createStatement();
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+//		try {
+////			((java.sql.Statement)stmt).executeUpdate("delete from friendlist where UserID in (select UserID from Users where Username ='"+username+"') and FriendID in (select UserID from Users where Username ='"+acceptUser+"')");
+//		} catch (SQLException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		try {
+////			((java.sql.Statement)stmt).executeUpdate("delete from friendlist where UserID in (select UserID from Users where Username ='"+acceptUser+"') and FriendID in (select UserID from Users where Username ='"+username+"')");
+//		} catch (SQLException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+		
+	}
 	public void initialize(String Username) {
-		this.setTitle("FRIEND OPTION -" + Username);
+		this.setTitle("CHAT APPLICATION");
         this.setSize(1000,600);
         getContentPane().setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//contentPane = new JPanel();
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		contentPane.setLayout(null);
+		//contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(54, 600));
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel.setBounds(0, 0, 54, 600);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		panel.setBackground(new Color(220, 220, 220));
+		JPanel panelMenu = new JPanel();
+		panelMenu.setPreferredSize(new Dimension(54,600 )); 
+		panelMenu.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelMenu.setBounds(0, 0, 54, 563);
+		//contentPane.add(panelMenu);
+		panelMenu.setLayout(null);
+		panelMenu.setBackground(new Color(220, 220, 220));
 		
 		Icon iconChatMenu = new ImageIcon("source/image/iconChatMenu.png");
 	    buttonChatMenu = new JButton(iconChatMenu);
@@ -179,98 +257,90 @@ public class FriendRequest extends JFrame implements ActionListener {
 	    buttonUserMenu.setOpaque(false);
 	    buttonUserMenu.setContentAreaFilled(false);
 	    
-	    panel.add(buttonChatMenu);
-        panel.add(buttonFriendMenu);
-        panel.add(buttonSettingMenu);
-        panel.add(buttonUserMenu);
+	    panelMenu.add(buttonChatMenu);
+        panelMenu.add(buttonFriendMenu);
+        panelMenu.add(buttonSettingMenu);
+        panelMenu.add(buttonUserMenu);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setBounds(54, 0, 337, 708);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel panelOption = new JPanel();
+		panelOption.setBorder(new LineBorder(new Color(0, 0, 0)));
+		 panelOption.setPreferredSize(new Dimension(350,600)); 
+		//contentPane.add(panelOption);
+		panelOption.setLayout(null);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_3.setBounds(0, 0, 340, 81);
-		panel_1.add(panel_3);
-		panel_3.setLayout(null);
+		JPanel panelTitle = new JPanel();
+		panelTitle.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelTitle.setBounds(0, 0,349, 81);
+		panelOption.add(panelTitle);
+		panelTitle.setLayout(null);
 		
-		JLabel lblNewJgoodiesLabel_4 = DefaultComponentFactory.getInstance().createLabel("    FRIENDS");
-		lblNewJgoodiesLabel_4.setFont(new Font("Times New Roman", Font.BOLD, 26));
-		lblNewJgoodiesLabel_4.setBounds(0, 0, 330, 81);
-		panel_3.add(lblNewJgoodiesLabel_4);
+		JLabel lbTitle = new JLabel("    FRIENDS");
+		lbTitle.setFont(new Font("Times New Roman", Font.BOLD, 26));
+		lbTitle.setBounds(0, 0, 330, 81);
+		panelTitle.add(lbTitle);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(0, 79, 327, 63);
-		panel_1.add(panel_2);
-		panel_2.setLayout(null);
+		JPanel panelSearch = new JPanel();
+		panelSearch.setBounds(0, 79, 327, 63);
+		panelOption.add(panelSearch);
+		panelSearch.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(10, 11, 202, 34);
-		panel_2.add(textField);
-		textField.setColumns(10);
+		JTextField txtSearch = new JTextField();
+		txtSearch.setBounds(10, 11, 202, 34);
+		panelSearch.add(txtSearch);
+		txtSearch.setColumns(10);
 		
-		JButton btnNewButton = new JButton("SEARCH");
-		btnNewButton.setBounds(222, 11, 108, 39);
-		panel_2.add(btnNewButton);
-		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		JButton btnSearch = new JButton("SEARCH");
+		btnSearch.setBounds(222, 11, 108, 39);
+		panelSearch.add(btnSearch);
+		btnSearch.setFont(new Font("Times New Roman", Font.BOLD, 18));
 		
-		btnFriendRequest = new JButton("Friend Request");
+		JButton btnFriendRequest = new JButton("Friend Request");
 		btnFriendRequest.setFont(new Font("Times New Roman", Font.PLAIN, 24));
-		btnFriendRequest.setIcon(new ImageIcon("D:\\eclipse-workspace\\ui\\Source\\Image\\add-user.png"));
+		btnFriendRequest.setIcon(new ImageIcon("source/image/add-user.png"));
 		
 		btnFriendRequest.setBounds(0, 153, 327, 75);
-		panel_1.add(btnFriendRequest);
+		panelOption.add(btnFriendRequest);
 		
-		btnFriendList = new JButton("Friend List");
-		btnFriendList.setIcon(new ImageIcon("D:\\eclipse-workspace\\ui\\Source\\Image\\friends.png"));
+		JButton btnFriendList = new JButton("Friend List");
+		btnFriendList.setIcon(new ImageIcon("source/image/friends.png"));
 		btnFriendList.setFont(new Font("Times New Roman", Font.PLAIN, 24));
 		btnFriendList.setBounds(0, 257, 327, 75);
+		panelOption.add(btnFriendList);
 		btnFriendList.addActionListener(this);
-		panel_1.add(btnFriendList);
-		
-		btnGroup = new JButton("Group");
-		btnGroup.setIcon(new ImageIcon("D:\\eclipse-workspace\\ui\\Source\\Image\\group.png"));
+		JButton btnGroup = new JButton("Group");
+		btnGroup.setIcon(new ImageIcon("source/image/group.png"));
 		btnGroup.setFont(new Font("Times New Roman", Font.PLAIN, 24));
 		btnGroup.setBounds(0, 362, 327, 75);
-		panel_1.add(btnGroup);
-		 
-		panel_6 = new JPanel();
-		panel_6.setBounds(391, 0, 596, 721);
-		contentPane.add(panel_6);
-		panel_6.setLayout(null);
 		
-		JPanel panel_8 = new JPanel();
-		panel_8.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_8.setBounds(0, 0, 596, 80);
-		panel_6.add(panel_8);
-		panel_8.setLayout(null);
+		btnGroup.addActionListener(this);
+		panelOption.add(btnGroup);
 		
-		JLabel lblNewJgoodiesLabel_11 = DefaultComponentFactory.getInstance().createLabel("");
-		lblNewJgoodiesLabel_11.setIcon(new ImageIcon("D:\\eclipse-workspace\\ui\\Source\\Image\\friends.png"));
-		lblNewJgoodiesLabel_11.setBounds(24, 0, 89, 82);
-		panel_8.add(lblNewJgoodiesLabel_11);
-		JLabel lblNewJgoodiesLabel_12 = DefaultComponentFactory.getInstance().createLabel("Friend's Request");
-		lblNewJgoodiesLabel_12.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		lblNewJgoodiesLabel_12.setBounds(101, 11, 260, 60);
-		panel_8.add(lblNewJgoodiesLabel_12);
+		panelView = new JPanel();
+        panelView.setLayout(null);
+      
+        panelView.setPreferredSize(new Dimension(596,600));
+        
+        JPanel panelOptionTitle = new JPanel();
+        panelOptionTitle.setLayout(null);
+        panelOptionTitle.setBorder(new LineBorder(new Color(0, 0, 0)));
+        panelOptionTitle.setBounds(0, 0, 596, 80);
+        panelView.add(panelOptionTitle);
+        
+        JLabel lbOptionTitle = new JLabel("Friend's Request");
+        lbOptionTitle.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        lbOptionTitle.setBounds(10, 11, 351, 60);
+        panelOptionTitle.add(lbOptionTitle);
+        
+       
 		
-		
-		
-		getContentPane().add(contentPane);
-		
-		
-		
-		
-		
+		getContentPane().add(panelMenu, BorderLayout.WEST);
+        getContentPane().add(panelOption, BorderLayout.CENTER);
+        getContentPane().add(panelView, BorderLayout.EAST);
 	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource() == buttonChatMenu) {
-			isOpen = false;
 			this.dispose();
             try{
                 new HomeScreen(username, client);
@@ -279,11 +349,10 @@ public class FriendRequest extends JFrame implements ActionListener {
             }
 		}
 		else if (e.getSource() == buttonFriendMenu) {
-			isOpen = false;
+			
 		}
 		else if (e.getSource() == buttonSettingMenu) {
 			try{
-				isOpen = false;
 //                new SettingScreen();
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -291,24 +360,23 @@ public class FriendRequest extends JFrame implements ActionListener {
 		}
 		else if (e.getSource() == buttonUserMenu) {
             try{
-            	isOpen = false;
 //                new UserSettingScreen();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
 		}
 		else if (e.getSource() == btnFriendList) {
-            try{
-            	isOpen = false;
-                new FriendList(username, client);
-                this.dispose();
+
+			this.dispose();
+			try{
+				new FriendList(username, client );
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
 		}
 		
 		else if (e.getSource() == btnGroup) {
-			isOpen = false;
+            
 		}
 		
 	}
