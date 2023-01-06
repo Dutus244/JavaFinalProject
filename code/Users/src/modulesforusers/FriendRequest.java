f97090eb91fa64a5b7dd87b2b7ea2d0277072c72
annthuu <80025852+anhthu872@users.noreply.github.com>
2023-01-06 20:35:43 +0700

    deleteGroupButton

--- code/Users/src/modulesforusers/FriendRequest.java
+++ code/Users/src/modulesforusers/FriendRequest.java
@@ -41,7 +41,7 @@ public class FriendRequest extends JFrame implements ActionListener {
 	JButton buttonUserMenu;
 	JButton btnFriendRequest;
 	JButton btnFriendList;
-	JButton btnGroup;
+	
 	JButton btnAccept;
 	JButton btnChat;
 	String username;
@@ -336,12 +336,7 @@ public class FriendRequest extends JFrame implements ActionListener {
 		btnFriendList.setBounds(0, 257, 327, 75);
 		panelOption.add(btnFriendList);
 		
-		btnGroup = new JButton("Group");
-		btnGroup.setIcon(new ImageIcon("source/image/group.png"));
-		btnGroup.setFont(new Font("Times New Roman", Font.PLAIN, 24));
-		btnGroup.setBounds(0, 362, 327, 75);
-		panelOption.add(btnGroup);
-		btnGroup.addActionListener(this);
+		
 		panelView = new JPanel();
         panelView.setLayout(null);
       
@@ -411,8 +406,6 @@ public class FriendRequest extends JFrame implements ActionListener {
             }
 		}
 		
-		else if (e.getSource() == btnGroup) {
-            
-		}
+		
 	}
 }
